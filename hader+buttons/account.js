03be0fe30942335ecea9918c1e4cfac23a0e632f
Farcal Andrei-Ioan <58657743+andreiioan94@users.noreply.github.com>
2021-04-05 18:57:31 +0300

    Add files via upload

---
+++ hader+buttons/account.js
@@ -0,0 +1,21 @@
+
+function displayAccountOptions(){
+    var x = document.getElementsByClassName("my_account_options")[0];
+    if(x.style.display === "none"){
+        x.style.display = "flex";
+    }
+}
+function hideAccountOptions(){
+    var x = document.getElementsByClassName("my_account_options")[0];
+    if(x.style.display === "flex"){
+        x.style.display = "none";
+    }
+}
+function displayLoginForm(){
+    var x = document.getElementById("login_overlay");
+    window.onclick = function(event){
+        if(event.target == x){
+            x.style.display = "none";
+        }
+    }
+}
