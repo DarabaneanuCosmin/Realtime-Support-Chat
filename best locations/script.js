9e9b4af10145f951c11cc0365ba79f5999ebc7ed
Modoranu Cosmin <zero6305@gmail.com>
2021-04-09 20:26:20 +0300

    Improved website look and feel

--- best locations/script.js
+++ best locations/script.js
@@ -3,8 +3,11 @@ var slidesPerRow = 3;
 
 var currentSlide = 0;
 
+
 var slidesContainer = document.querySelector(".popular__cardcontainer");
 var slides = slidesContainer.querySelectorAll(".popular__imagecard");
+
+
 
 console.log(slides.length);
 
@@ -14,7 +17,6 @@ switchSlide(0);
 function switchSlide(n){
 
 
-    //@Refactorizare - prea incet in continuare
     if(currentSlide + n + slidesPerRow > slides.length || currentSlide + n < 0){
         console.log("skipped, current slide was " + currentSlide + " and n was" + n);
         return;
