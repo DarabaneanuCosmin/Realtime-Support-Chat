02e690b88abbee8a2955f2ad55d39bf36002473a
Dărăbăneanu Cosmin <darabaneanucosmin81@gmail.com>
2021-05-26 17:45:11 +0300

    update addNewMessages()

--- API/app.js
+++ API/app.js
@@ -36,21 +36,21 @@ createAdminTable();
 createUserIdSequence();
 createMessageIdSequence();
 
-const server = http.createServer(async (req, res) => {
+const server = http.createServer(async(req, res) => {
 
     const corsHeaders = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
         "Access-Control-Max-Age": 2592000, // 30 days
         "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-      };
+    };
 
 
     if (req.url === '/api/admins' && req.method === 'GET') {
         //-url:/api/admins si metoda:GET- luam lista adminilor si verificam statusul lor
         getAdminList(req, res);
-    } else if (req.url.match(/\/api\/messages\/([a-z A-Z 0-9]+)\/([a-z A-Z 0-9]+)/) 
-    && req.method === 'GET') {
+    } else if (req.url.match(/\/api\/messages\/([a-z A-Z 0-9]+)\/([a-z A-Z 0-9]+)/) &&
+        req.method === 'GET') {
         let idRoom = req.url.split('/')[4];
         let idUser = req.url.split('/')[3];
 
@@ -60,7 +60,7 @@ const server = http.createServer(async (req, res) => {
         const body = await getPostData(req);
         var parseMessage = JSON.parse(body);
         createSession(req, res, parseMessage.session_id,
-         parseMessage.create_date);
+            parseMessage.create_date);
 
     } else if (req.url.match(/\/api\/session\/([a-z A-Z 0-9]+)/) && req.method === 'GET') {
         //luam sesiunea ce are session_id = ...
@@ -74,16 +74,16 @@ const server = http.createServer(async (req, res) => {
 
         createRoom(req, res, parseMessage.sessionId);
 
-    } else if (req.url.match(/\/api\/messages\/showRooms\/([a-z A-Z 0-9]+)/)
-        && req.method === 'GET') {
+    } else if (req.url.match(/\/api\/messages\/showRooms\/([a-z A-Z 0-9]+)/) &&
+        req.method === 'GET') {
         // /api/messages/showRooms/:idUser si metoda GET - luam room-ul in care este userul idUser
 
         const idUser = req.url.split('/')[4];
         console.log(idUser);
         getRoomByUserId(req, res, idUser);
 
-    }  else if (req.url.match(/\/api\/messages\/([a-z A-Z 0-9]+)\/([a-z A-Z 0-9]+)/)
-        && req.method === 'GET') {
+    } else if (req.url.match(/\/api\/messages\/([a-z A-Z 0-9]+)\/([a-z A-Z 0-9]+)/) &&
+        req.method === 'GET') {
         // :/api/messages/:idRoom/:idClient1 si metoda:GET - 
         //luam toate datele conversatiei din Room-ul indicat pentru userul cu id-ul idClient1
 
@@ -106,8 +106,8 @@ const server = http.createServer(async (req, res) => {
         console.log(idRoom);
         deleteRoom(req, res, idRoom);
 
-    } else if (req.url.match(/\/api\/messages\/([a-z A-Z 0-9]+)\/([a-z A-Z 0-9]+)/)
-        && req.method === 'POST') {
+    } else if (req.url.match(/\/api\/messages\/([a-z A-Z 0-9]+)\/([a-z A-Z 0-9]+)/) &&
+        req.method === 'POST') {
         //url:/api/messages/:idRoom/:idClient1 si metoda:POST - 
         //adaugam in Room-ul idRoom mesajele noi trimise de catre idClient
 
@@ -120,8 +120,8 @@ const server = http.createServer(async (req, res) => {
 
         addNewMessages(req, res, idRoom, parseMessage.clientMessage, sessionId);
 
-    } else if(req.method === 'OPTIONS'){
-        res.writeHead(204, 
+    } else if (req.method === 'OPTIONS') {
+        res.writeHead(204,
             corsHeaders);
         res.end();
         return;
--- API/service/service.js
+++ API/service/service.js
@@ -3,77 +3,6 @@ const utils = require("../utils/utils");
 const util = require("util");
 const generateUniqueId = require('generate-unique-id');
 
-/*
-async function createRoom(req, res, idUser) {
-    try {
-        //   const value = await db.existsUserInJoinByIdUser(idUser, information);
-        /*
-          const findSDASDA = await db.pool.query(...{ //result = idUser
-              if (error) {
-                  throw error;
-              } else {
-                  idUser
-          })
-          */
-/*
-        const resultQuery = await db.pool.query("SELECT idRoom from JOINMESSAGES  WHERE idUser = ?", [idUser], function(error, result) {
-            if (error) {
-                throw error;
-            } else {
-                if (result[0] != null) {
-                    console.log(result[0]);
-                    let message = result[0];
-                    res.writeHead(200, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify(message));
-                } else {
-
-                    const id = utils.between(100, 12000000000);
-                    db.insertIntoTable("INSERT INTO room VALUES (?,?)", [id, null]);
-                    db.insertIntoTable("INSERT INTO joinmessages VALUES(?,?)", [id, idUser]);
-                    let message = { message: "Room-ul nu exista. Adaugam unul!", idRoom: id };
-                    res.writeHead(200, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify(message));
-                }
-            }
-        });
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-*/
-/*
-x2
-    if (idUser != undefined) {
-                    const resultQuery = await db.pool.query("SELECT idRoom from JOINMESSAGES  WHERE idUser = ?", [value], async function(error, result) {
-                        if (error) {
-                            throw error;
-                        } else {
-                            console.log(result[0]);
-                            if (result[0] != null) {
-                                console.log(result[0]);
-                                let message = { message: "Room-ul exista!", idRoom: result[0] };
-                                res.writeHead(200, { 'Content-Type': 'application/json' });
-                                res.end(JSON.stringify(message));
-                            } else {
-
-                                const id = utils.between(100, 12000000000);
-                                db.insertIntoTable("INSERT INTO room VALUES (?,?)", [id, null]);
-                                db.insertIntoTable("INSERT INTO joinmessages VALUES(?,?)", [id, idUser]);
-                                let message = { message: "Room-ul nu exista! Adaugam unul!", idRoom: id };
-                                res.writeHead(200, { 'Content-Type': 'application/json' });
-                                res.end(JSON.stringify(message));
-                            }
-                        }
-                    });
-                } else {
-                    let message = { message: "SessionId nu exista!" };
-                    res.writeHead(200, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify(message));
-                }
-
-*/
-
 
 async function createRoom(req, res, sessionId) {
     try {
@@ -86,8 +15,10 @@ async function createRoom(req, res, sessionId) {
                     await getIdRoomByIdUser(req, res, idUser.idUser);
                 } else {
                     let message = { message: `SessionId : ${sessionId} does not exist in db!` };
-                    res.writeHead(404, { 'Content-Type': 'application/json',
-                    "Access-Control-Allow-Origin": "*"});
+                    res.writeHead(404, {
+                        'Content-Type': 'application/json',
+                        "Access-Control-Allow-Origin": "*"
+                    });
                     res.end(JSON.stringify(message));
                 }
             }
@@ -105,17 +36,21 @@ async function getIdRoomByIdUser(req, res, idUser) {
             } else {
 
                 if (result[0] != undefined) {
-                    let message = { idRoom: result[0].idRoom, message: 'Returning already created room!'};
-                    res.writeHead(200, { 'Content-Type': 'application/json',
-                    "Access-Control-Allow-Origin": "*" });
+                    let message = { idRoom: result[0].idRoom, message: 'Returning already created room!' };
+                    res.writeHead(200, {
+                        'Content-Type': 'application/json',
+                        "Access-Control-Allow-Origin": "*"
+                    });
                     res.end(JSON.stringify(message));
                 } else {
                     const id = utils.between(100, 12000000000);
                     db.insertIntoTable("INSERT INTO room VALUES (?,?)", [id, null]);
                     db.insertIntoTable("INSERT INTO joinmessages VALUES(?,?)", [id, idUser]);
                     let message = { message: "The room does not exist! A new one is added!", idRoom: id };
-                    res.writeHead(201, { 'Content-Type': 'application/json',
-                    "Access-Control-Allow-Origin": "*" });
+                    res.writeHead(201, {
+                        'Content-Type': 'application/json',
+                        "Access-Control-Allow-Origin": "*"
+                    });
                     res.end(JSON.stringify(message));
                 }
 
@@ -135,15 +70,13 @@ async function fetchMessages(req, res, idRoom, idSession) {
     var messages = [];
 
     selectFrom("SELECT idUser from session WHERE sessionId = ?", [idSession])
-        .then(async (rows) => {
+        .then(async(rows) => {
             if (rows.length <= 0) {
                 throw "Invalid user id!";
             }
 
-            return await selectFrom("SELECT * FROM joinmessages WHERE idRoom = ?",
-                [idRoom]);
-        }
-        ).then(async (rows) => {
+            return await selectFrom("SELECT * FROM joinmessages WHERE idRoom = ?", [idRoom]);
+        }).then(async(rows) => {
 
             console.log(rows);
             if (rows.length <= 0) {
@@ -152,26 +85,29 @@ async function fetchMessages(req, res, idRoom, idSession) {
 
             idUser = rows[0].idUser;
 
-            return await selectFrom("SELECT* FROM messages WHERE idRoom = ?",
-                [rows[0].idRoom])
+            return await selectFrom("SELECT* FROM messages WHERE idRoom = ?", [rows[0].idRoom])
         }).then((nextRows) => {
             nextRows.forEach((element) => {
-                newElement = {...element, 
-                    sentByMe: (idUser == element.idUser) ? true : false};
+                newElement = {...element,
+                    sentByMe: (idUser == element.idUser) ? true : false
+                };
                 messages.push(newElement)
-            }
-            );
+            });
             console.log(messages);
 
-            res.writeHead(200, { 'Content-Type': 'application/json',
-            "Access-Control-Allow-Origin" : "*" });
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Origin": "*"
+            });
             res.end(JSON.stringify(messages));
         })
         .catch((err) => {
             console.log(res);
 
-            res.writeHead(404, { 'Content-Type': 'application/json',
-            "Access-Control-Allow-Origin": "*" });
+            res.writeHead(404, {
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Origin": "*"
+            });
             res.end(JSON.stringify({ message: err, error: 1 }));
         });
 }
@@ -196,48 +132,65 @@ async function addUserInRoom(req, res, idRoom, idUser) {
     }
 }
 
+// @desc add a new message to the database
+// @route POST /api/messages/{:idRoom}/{:sessionId} with body : {"clientMessage" : ":clientMessage"}
 async function addNewMessages(req, res, idRoom, clientMessage, sessionId) {
     try {
-        const getSessionId = await db.pool.query("SELECT idUser from session WHERE sessionId = ?",
-            [sessionId], async function (error, resultQuery) {
-                if (error) {
-                    throw error;
-                } else {
-                    console.log(resultQuery[0]);
-                    if (resultQuery[0] != undefined) {
-                        const idUser = resultQuery[0].idUser;
+        const getSessionId = await db.pool.query("SELECT idUser from session WHERE sessionId = ?", [sessionId], async function(error, resultQuery) {
+            if (error) {
+                throw error;
+            } else {
+                console.log(resultQuery[0]);
+                if (resultQuery[0] != undefined) {
+                    const idUser = resultQuery[0].idUser;
 
-                        const resultQuery1 = await db.pool.query("SELECT idRoom, idUser from JOINMESSAGES  WHERE idUser = ?", [idUser], function (error, result) {
-                            if (error) {
-                                throw error;
-                            } else {
+                    const resultQuery1 = await db.pool.query("SELECT idRoom, idUser from JOINMESSAGES  WHERE idUser = ?", [idUser], function(error, result) {
+                        if (error) {
+                            throw error;
+                        } else {
 
-                                if (result.length > 0) {
+                            if (result.length > 0) {
+                                if (result[0].idRoom == idRoom && result[0].idUser == idUser) {
                                     const now = new Date();
-                                    db.insertIntoTable("INSERT INTO messages VALUES (nextval(messageIdSeq), ?,?,?,?)", [idRoom, idUser, clientMessage, now.getTime()], "messages");
+
+                                    db.insertIntoTable("INSERT INTO messages VALUES (nextval(messageIdSeq), ?,?,?,?)", [idRoom, idUser, clientMessage, now.toLocaleTimeString()], "messages");
                                     const todos = { message: "Mesajul a fost adaugat cu succes!" };
-                                    res.writeHead(200, { 'Content-Type': 'application/json',
-                                    "Access-Control-Allow-Origin": "*" });
+                                    res.writeHead(200, {
+                                        'Content-Type': 'application/json',
+                                        "Access-Control-Allow-Origin": "*"
+                                    });
                                     res.end(JSON.stringify(todos));
                                 } else {
-                                    const todos = { message: "Userul nu exista in lobby-ul indicat" };
-                                    res.writeHead(400, { 'Content-Type': 'application/json',
-                                    "Access-Control-Allow-Origin": "*" });
+                                    const todos = { message: "The user doesn't have access here or the room doesn't exist !" };
+                                    res.writeHead(400, {
+                                        'Content-Type': 'application/json',
+                                        "Access-Control-Allow-Origin": "*"
+                                    });
                                     res.end(JSON.stringify(todos));
                                 }
 
 
+                            } else {
+                                const todos = { message: "The received roomID/ iUser should match with the informations from  database" };
+                                res.writeHead(404, {
+                                    'Content-Type': 'application/json',
+                                    "Access-Control-Allow-Origin": "*"
+                                });
+                                res.end(JSON.stringify(todos));
                             }
-                        });
+                        }
+                    });
 
-                    } else {
-                        let message = { message: "idUser nu exista" };
-                        res.writeHead(200, { 'Content-Type': 'application/json',
-                        "Access-Control-Allow-Origin": "*" });
-                        res.end(JSON.stringify(message));
-                    }
+                } else {
+                    let message = { message: `SessionId : ${sessionId} does not exist in db!` };
+                    res.writeHead(404, {
+                        'Content-Type': 'application/json',
+                        "Access-Control-Allow-Origin": "*"
+                    });
+                    res.end(JSON.stringify(message));
                 }
-            });
+            }
+        });
 
     } catch (error) {
         console.log(error);
@@ -307,13 +260,12 @@ async function createSession(req, res, session_id, create_date) {
     var selectFrom = util.promisify(db.pool.query).bind(db.pool);
 
     selectFrom("SELECT idUser from session WHERE sessionId = ?", [session_id])
-        .then(async (rows) => {
+        .then(async(rows) => {
             if (rows.length > 0) {
 
                 throw "Sesiunea are asignata un user deja";
             }
-            db.insertIntoTable("INSERT INTO session (sessionId, idUser, create_date) VALUES (?, nextval(userIdSeq), ?)",
-                [session_id, idUser, create_date], "session");
+            db.insertIntoTable("INSERT INTO session (sessionId, idUser, create_date) VALUES (?, nextval(userIdSeq), ?)", [session_id, idUser, create_date], "session");
             const mesajReturn = { message: `Sesiune inserata cu success (sesId ${session_id}, createDate ${create_date})`, idUser }
             res.writeHead(201, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify(mesajReturn));
