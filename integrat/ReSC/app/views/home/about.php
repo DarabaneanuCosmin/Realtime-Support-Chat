f9d19a4a05f224f5c6e3561ad0579cd0cab5ec04
Farcal Andrei-Ioan <andreifarcal@yahoo.com>
2021-06-07 19:27:31 +0300

    second bugFix CSS

--- integrat/ReSC/app/views/home/about.php
+++ integrat/ReSC/app/views/home/about.php
@@ -12,19 +12,19 @@
 <body>
     <div class= "page">
         <div class= "element">
-            <a href="/public/home/documentation/"><button class="btn-hover color-3">Documentation</button></a>
+            <a href="/public/home/documentation/"><button class="aboutBtn color-3">Documentation</button></a>
             <img class= "about" src = "/public/img/documentationIMG.jpg" alt="photo">
         </div>
         <div class= "element">
-            <a href="/public/home/swagger/"><button class="btn-hover color-1">Swagger</button></a>
+            <a href="/public/home/swagger/"><button class="aboutBtn color-1">Swagger</button></a>
             <img class= "about" src = "/public/img/swagger.jpg" alt="photo">
         </div>
         <div class= "element">
-            <a href="/public/home/about/"><button class="btn-hover color-4">Arhitecure</button></a>
+            <a href="/public/home/about/"><button class="aboutBtn color-4">Arhitecure</button></a>
             <img class= "about" src = "/public/img/arhitecture.jpg" alt="photo">
         </div>
         <div class= "element">
-            <a href="/public/home/about/"><button class="btn-hover color-11">Video</button></a>
+            <a href="/public/home/about/"><button class="aboutBtn color-11">Video</button></a>
             <img class= "about" src = "/public/img/video.jpg" alt="photo">
         </div>
     </div>
